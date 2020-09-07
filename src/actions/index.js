16c0ccfd5c2ef23c0d500baf49941f767df64e93
akshatsinghkaushik <akshatsingh99@gmail.com>
2020-09-08 04:34:58 +1000

    Added simple UI and Redux to PostList Component

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => {
+  return async (dispatch, getState) => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  };
+};
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const PostList = () => {
-  return <div>Div</div>;
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+const PostList = ({ posts, fetchPosts }) => {
+  useEffect(() => {
+    fetchPosts();
+  }, [fetchPosts]);
+
+  console.log(posts);
+
+  const renderList = () => {
+    return posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return <div className="ui relaxed divided list">{renderList()}</div>;
 };
 
-export default PostList;
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
